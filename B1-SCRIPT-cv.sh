d152f36fa4d8a10f4e0ffec24a11c7cc6e181e78
tar1971 <96209005+tar1971@users.noreply.github.com>
2025-10-18 21:49:12 +0300

    Add files via upload

---
+++ B1-SCRIPT-cv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+wget -O /tmp/script-a.tar.gz "https://raw.githubusercontent.com/tar1971/settings/main/script-a.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/script-a.tar.gz
+
+killall -9 enigma2
+
+sleep 2;
+exit
